8f79e679772748575ed96b2abd00d99ed261e9bb
Rita_Achba <achba.rita@yandex.ru>
2022-02-28 23:56:00 +0300

    28.02.2022 Slider: HTML, CSS

---
+++ index.js
@@ -0,0 +1,19 @@
+const linkAdmiral = document.querySelector(".admiral");
+const linkThieves = document.querySelector(".thieves");
+const linkPatriotic = document.querySelector(".patriotic");
+
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const firstCircle = document.querySelector(".circle-1");
+const secondCircle = document.querySelector(".circle-2");
+const thirdCircle = document.querySelector(".circle-3");
+
+
+function sliderFunc () {
+    
+}
+
+
+
+
+window.addEventListener("load", sliderFunc)
